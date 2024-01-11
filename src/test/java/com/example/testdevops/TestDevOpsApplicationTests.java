caa1cdef4f0e5c952e43d8bc2451147b6c92bfbd
Baburam Neupane <neupanebabu828@gmail.com>
2024-01-11 19:22:18 +0545

    initialization

---
+++ src/test/java/com/example/testdevops/TestDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
